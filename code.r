1ab83d357e6551bc973edfa6bd010b82c0fb8af8
Camila Muñoz <cjmunozn@hotmail.com>
2024-12-12 23:07:49 -0300

    feat(R): Check if the folder exists; if not, create it

--- code.r
+++ code.r
@@ -1,3 +1,11 @@
 # Create a folder to store the information
 # Define the folder name
 folder <- "./R-1st-approach/output_data"
+
+# Check if the folder exists; if not, create it
+if (!dir.exists(folder)) {
+  dir.create(folder)
+  cat("Folder created:", folder, "\n")
+} else {
+  cat("Folder already exists:", folder, "\n")
+}
